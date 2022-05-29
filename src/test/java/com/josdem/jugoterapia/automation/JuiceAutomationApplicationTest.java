efa1be7e85e2afc48977e0045ec5f38fb4aae168
josdem <joseluis.delacruz@gmail.com>
2022-05-29 18:16:42 -0400

    #5 Adding missing assertion

--- src/test/java/com/josdem/jugoterapia/automation/JuiceAutomationApplicationTest.java
+++ src/test/java/com/josdem/jugoterapia/automation/JuiceAutomationApplicationTest.java
@@ -1,19 +1,28 @@
 package com.josdem.jugoterapia.automation;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @Slf4j
 @SpringBootTest
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class JuiceAutomationApplicationTest {
+
+  private final ApplicationContext applicationContext;
 
   @Test
   @DisplayName("it loads the application")
   void shouldLoadApplication(TestInfo testInfo) {
     log.info("Running {}", testInfo.getDisplayName());
     JuiceAutomationApplication.main(new String[] {});
+    assertNotNull(applicationContext, "it should have a context");
   }
 }
