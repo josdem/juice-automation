425f72c82274b4f7bc275ea22f2510c99a4b223e
josdem <joseluis.delacruz@gmail.com>
2022-05-29 18:10:35 -0400

    #5 Calling main application

--- src/test/java/com/josdem/jugoterapia/automation/JuiceAutomationApplicationTest.java
+++ src/test/java/com/josdem/jugoterapia/automation/JuiceAutomationApplicationTest.java
@@ -1,27 +1,19 @@
 package com.josdem.jugoterapia.automation;
 
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Slf4j
 @SpringBootTest
-@RequiredArgsConstructor(onConstructor_ = @Autowired)
 class JuiceAutomationApplicationTest {
 
-    private final ApplicationContext applicationContext;
-
-    @Test
-    @DisplayName("it loads the application")
-    void shouldLoadApplication(TestInfo testInfo){
-        log.info("Running {}", testInfo.getDisplayName());
-        assertNotNull(applicationContext, "it should exist a context");
-    }
+  @Test
+  @DisplayName("it loads the application")
+  void shouldLoadApplication(TestInfo testInfo) {
+    log.info("Running {}", testInfo.getDisplayName());
+    JuiceAutomationApplication.main(new String[] {});
+  }
 }
